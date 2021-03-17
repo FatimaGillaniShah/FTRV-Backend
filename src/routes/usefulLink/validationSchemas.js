49f09bdd3bf0c14526d465ebe82d093baddd7a9b
Danyal Ahmad <danyal.ahmad@datics.ai>
2021-03-17 13:47:23 +0500

    ftrv-2beyhr minor update

--- src/routes/usefulLink/validationSchemas.js
+++ src/routes/usefulLink/validationSchemas.js
@@ -1,10 +1,5 @@
 import Joi from 'joi';
 
-export const userLoginSchema = Joi.object().keys({
-  email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-  password: Joi.string().required(),
-});
-
 export const linkCreateSchema = Joi.object()
   .keys({
     name: Joi.string().required(),
