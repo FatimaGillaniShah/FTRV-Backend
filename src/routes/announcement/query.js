0e0139203632b3ca4d29ca5eef7be2753868c3eb
Danyal Ahmad <danyal.ahmad@datics.ai>
2021-03-25 13:34:06 +0500

    adding annoncements apis

--- src/routes/announcement/query.js
+++ src/routes/announcement/query.js
@@ -1,17 +1,22 @@
 import sequelize from 'sequelize';
-import moment from 'moment';
 
 const { Op } = sequelize;
 
-export const listQuery = ({ status }) => {
+export const listQuery = ({ status, sortColumn, sortOrder, pageNumber, pageSize }) => {
   const query = { where: {} };
   query.attributes = { exclude: ['createdAt', 'updatedAt', 'deletedAt'] };
 
-  const currentTime = moment().format('YYYY-MM-DD');
   if (status) {
-    query.where[Op.and] = [
-      { status, startTime: { [Op.lte]: currentTime }, endTime: { [Op.gte]: currentTime } },
-    ];
+    query.where[Op.and] = [{ status }];
+  }
+
+  // for pagination
+  query.offset = (pageNumber - 1) * pageSize;
+  query.limit = pageSize;
+
+  // for sorting
+  if (sortColumn && sortOrder) {
+    query.order = [[sortColumn, sortOrder]];
   }
 
   query.order = [['startTime', 'ASC']];
