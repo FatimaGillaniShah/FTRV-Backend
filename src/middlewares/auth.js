62a7950668412f569217ff4c2df3e2630a023f57
Usman Qamar <usmanqamar189@hotmail.com>
2021-04-27 15:29:11 +0500

    reverted auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -14,7 +14,6 @@ const auth = {
   required: jwt({
     secret: process.env.JWT_SECRET,
     getToken: getTokenFromHeaders,
-    credentialsRequired: false,
   }),
   optional: jwt({
     secret: process.env.JWT_SECRET,
