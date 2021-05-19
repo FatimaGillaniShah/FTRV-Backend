221cdf6143706d90469cd6248eb4da8cdc7b4db7
Hamza <ali.hamza@datics.ai>
2021-05-19 17:50:00 +0500

    schema updated

--- src/routes/usefulLink/validationSchemas.js
+++ src/routes/usefulLink/validationSchemas.js
@@ -2,14 +2,16 @@ import Joi from 'joi';
 
 export const linkCreateSchema = Joi.object()
   .keys({
-    name: Joi.string().required(),
+    name: Joi.string(),
     url: Joi.string().required(),
+    categoryId: Joi.number().required(),
   })
   .unknown(true);
 
 export const linkUpdateSchema = Joi.object()
   .keys({
     name: Joi.string(),
-    url: Joi.string(),
+    url: Joi.string().required(),
+    categoryId: Joi.number().required(),
   })
   .unknown(true);
