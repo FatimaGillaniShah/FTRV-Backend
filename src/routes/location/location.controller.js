5f892644c6f251891e29df2ef02803cbed1ee1a8
fatimashah5184 <fatimashah5184@gmail.com>
2021-05-26 13:35:59 +0600

    edit and delete api added

--- src/routes/location/location.controller.js
+++ src/routes/location/location.controller.js
@@ -4,7 +4,7 @@ import models from '../../models';
 import { BadRequestError, getErrorMessages, SuccessResponse } from '../../utils/helper';
 import { listQuery } from './query';
 import { STATUS_CODES } from '../../utils/constants';
-import { locationCreateSchema } from './validationSchemas';
+import { locationCreateSchema, locationUpdateSchema } from './validationSchemas';
 
 const { Location } = models;
 class LocationController {
@@ -14,6 +14,8 @@ class LocationController {
     this.router = express.Router();
     this.router.get('/', this.list);
     this.router.post('/', this.createLocation);
+    this.router.put('/:id', this.updateLocation);
+    this.router.delete('/deleteLocations', this.deleteLocations);
 
     return this.router;
   }
@@ -47,6 +49,53 @@ class LocationController {
       const location = await Location.create(locationPayload);
       const locationResponse = location.toJSON();
       return SuccessResponse(res, locationResponse);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async updateLocation(req, res, next) {
+    const {
+      body: locationPayload,
+      params: { id: locationId },
+    } = req;
+    try {
+      const result = Joi.validate(locationPayload, locationUpdateSchema);
+      if (result.error) {
+        BadRequestError(getErrorMessages(result), STATUS_CODES.INVALID_INPUT);
+      }
+      const query = {
+        where: {
+          id: locationId,
+        },
+      };
+
+      const locationtExists = await Location.findOne(query);
+      if (locationtExists) {
+        const location = await Location.update(locationPayload, query);
+        return SuccessResponse(res, location);
+      }
+      BadRequestError(`Location does not exists`, STATUS_CODES.NOTFOUND);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async deleteLocations(req, res, next) {
+    const {
+      body: { ids = [] },
+    } = req;
+    try {
+      if (ids.length < 1) {
+        BadRequestError(`Location ids required`, STATUS_CODES.INVALID_INPUT);
+      }
+      const locations = await Location.destroy({
+        where: {
+          id: ids,
+        },
+        force: true,
+      });
+      return SuccessResponse(res, { count: locations });
     } catch (e) {
       next(e);
     }
--- src/routes/location/validationSchemas.js
+++ src/routes/location/validationSchemas.js
@@ -3,3 +3,8 @@ import Joi from 'joi';
 export const locationCreateSchema = Joi.object().keys({
   name: Joi.string().required(),
 });
+export const locationUpdateSchema = Joi.object()
+  .keys({
+    name: Joi.string(),
+  })
+  .unknown(true);
