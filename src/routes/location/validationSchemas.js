0bc8d6db205a297fe42706f430905b1cef4bbf08
fatimashah5184 <fatimashah5184@gmail.com>
2021-05-26 16:33:28 +0600

    feedback changes

--- src/routes/location/validationSchemas.js
+++ src/routes/location/validationSchemas.js
@@ -1,10 +1,5 @@
 import Joi from 'joi';
 
-export const locationCreateSchema = Joi.object().keys({
+export const locationSchema = Joi.object().keys({
   name: Joi.string().required(),
 });
-export const locationUpdateSchema = Joi.object()
-  .keys({
-    name: Joi.string(),
-  })
-  .unknown(true);
