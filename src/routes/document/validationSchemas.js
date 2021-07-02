b2cbac336cb292672bee53a831954a3dec4d39fd
Hamza <ali.hamza@datics.ai>
2021-07-02 18:22:24 +0500

    name marked as optional

--- src/routes/document/validationSchemas.js
+++ src/routes/document/validationSchemas.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const documentCreateSchema = Joi.object()
   .keys({
-    name: Joi.string().required(),
+    name: Joi.string().optional(),
     description: Joi.string().optional(),
     departmentId: Joi.number().required(),
   })
