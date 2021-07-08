a5dc64b6b473aca0d96fea05381d42f1e79837f1
Hamza <ali.hamza@datics.ai>
2021-07-08 12:15:12 +0500

    feedback addressed

--- src/routes/document/validationSchemas.js
+++ src/routes/document/validationSchemas.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const documentCreateSchema = Joi.object()
   .keys({
-    name: Joi.string().optional(),
+    name: Joi.string().required(),
     description: Joi.string().optional(),
     departmentId: Joi.number().required(),
   })
