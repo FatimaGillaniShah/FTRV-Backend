2cf7ad80165aadaf024ce0524bfc23cd2c427ed8
Hamza <ali.hamza@datics.ai>
2021-07-13 19:07:13 +0500

    location and department included

--- src/routes/ringGroup/query.js
+++ src/routes/ringGroup/query.js
@@ -1,10 +1,29 @@
+import models from '../../models';
+
+const { Location, Department } = models;
 export const getRingGroupByIdQuery = (ringGroupId) => {
   const query = {};
   query.where = {
     id: ringGroupId,
   };
+  query.include = [
+    {
+      model: Location,
+      as: 'location',
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    },
+    {
+      model: Department,
+      as: 'department',
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    },
+  ];
   query.attributes = {
-    exclude: ['createdAt', 'updatedAt'],
+    exclude: ['createdAt', 'updatedAt', 'locationId', 'departmentId'],
   };
   return query;
 };
