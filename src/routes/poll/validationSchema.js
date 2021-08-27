c19eb1b6da5ff9dff5f801aba656f7872f91d281
Hamza <ali.hamza@datics.ai>
2021-08-27 13:07:32 +0500

    poll date format

--- src/routes/poll/validationSchema.js
+++ src/routes/poll/validationSchema.js
@@ -5,8 +5,8 @@ export const createPollSchema = ({ body, query: { date = new Date() } }) => {
   const schema = Joi.object()
     .keys({
       question: Joi.string().required(),
-      startDate: Joi.date().required().min(moment(date).format('YYYY-MM-DD')),
-      endDate: Joi.date().required().min(moment(date).format('YYYY-MM-DD')),
+      startDate: Joi.date().required().min(moment(date).format('MM-DD-YYYY')),
+      endDate: Joi.date().required().min(moment(date).format('MM-DD-YYYY')),
       status: Joi.string().required(),
       options: Joi.array().items(Joi.string()).min(2).max(4).required(),
     })
@@ -18,8 +18,8 @@ export const updatePollSchema = ({ body, query: { date = new Date() } }) => {
   const schema = Joi.object()
     .keys({
       question: Joi.string().optional(),
-      startDate: Joi.date().optional().min(moment(date).format('YYYY-MM-DD')),
-      endDate: Joi.date().optional().min(moment(date).format('YYYY-MM-DD')),
+      startDate: Joi.date().optional().min(moment(date).format('MM-DD-YYYY')),
+      endDate: Joi.date().optional().min(moment(date).format('MM-DD-YYYY')),
       status: Joi.string().optional(),
       options: Joi.array().items().min(2).max(4).optional(),
     })
