e5e3e4198d7e71cd8220273a60742a160b74132a
Hamza <ali.hamza@datics.ai>
2021-09-09 18:17:35 +0500

    delete profit center api, swagger docs

--- src/routes/profitCenter/profitCenter.controller.js
+++ src/routes/profitCenter/profitCenter.controller.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { SuccessResponse } from '../../utils/helper';
+import { BadRequestError, SuccessResponse } from '../../utils/helper';
 import models from '../../models';
 import { Request } from '../../utils/decorators';
-import { PAGE_SIZE } from '../../utils/constants';
-import { listProfitCentersQuery } from './query';
+import { PAGE_SIZE, STATUS_CODES } from '../../utils/constants';
+import { deleteProfitCenterQuery, listProfitCentersQuery } from './query';
 
 const { ProfitCenter } = models;
 
@@ -13,6 +13,7 @@ class ProfitCenterController {
   static getRouter() {
     this.router = express.Router();
     this.router.get('/', this.list);
+    this.router.delete('/', this.deleteProfitCenter);
 
     return this.router;
   }
@@ -31,6 +32,19 @@ class ProfitCenterController {
     });
     const profitCenters = await ProfitCenter.findAndCountAll(query);
     return SuccessResponse(res, profitCenters);
+  }
+
+  @Request
+  static async deleteProfitCenter(req, res) {
+    const {
+      body: { ids: profitCenterIds = [] },
+    } = req;
+
+    if (profitCenterIds.length < 1) {
+      BadRequestError(`Profit Center id is required`, STATUS_CODES.INVALID_INPUT);
+    }
+    const profitCenterCount = await ProfitCenter.destroy(deleteProfitCenterQuery(profitCenterIds));
+    return SuccessResponse(res, { count: profitCenterCount });
   }
 }
 export default ProfitCenterController;
